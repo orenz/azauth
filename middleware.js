fcfc6bad59a93b841b3630b627cb80044c9ab5af
EdwardTulane1 <58666649+EdwardTulane1@users.noreply.github.com>
2021-10-07 13:02:37 +0300

    zz

--- middleware.js
+++ middleware.js
@@ -1,3 +1,4 @@
+
 const jwt = require('jsonwebtoken');
 const fetch = require('node-fetch');
 
@@ -27,6 +28,6 @@ function auth(req, res, next){
 
 async function init(){
     
-    publicKey=await (await fetch('http://auth.azjs.io/api/publicKey')).text();
+    publicKey=await (await fetch('https://auth.azjs.io/api/publicKey')).text();
 }
 module.exports.auth=auth;
